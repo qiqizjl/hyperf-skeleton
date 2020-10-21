e5e949fad3eaff593101a9f15c614ebf8862caf0
耐小心 <qiqizjl@qq.com>
2020-10-21 10:49:02 +0800

    Update config.php
    更新ide-helper依赖版本

--- installer/config.php
+++ installer/config.php
@@ -59,7 +59,7 @@ return [
             'version' => '~2.0.0',
         ],
         'naixiaoxin/hyperf-ide-helper' => [
-            'version' => '~1.0.0',
+            'version' => '~1.1.0',
         ],
     ],
     'require-dev' => [
